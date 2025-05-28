cb76876e1c5e4aaa513093b8a1c1750587161e60
Quentinc63 <layk63@gmail.com>
2025-05-28 16:57:47 +0200

    feat: add fk for most of my entity

--- src/main/java/com/humanbooster/demo/service/BorneService.java
+++ src/main/java/com/humanbooster/demo/service/BorneService.java
@@ -1,5 +1,31 @@
 package com.humanbooster.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.humanbooster.demo.model.Borne;
+import com.humanbooster.demo.repository.BorneRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
 public class BorneService {
+    private final BorneRepository borneRepository;
+
+    public BorneService(BorneRepository borneRepository) {
+        this.borneRepository = borneRepository;
+    }
+    @Transactional
+    public void deleteAllBorne() {
+        borneRepository.deleteAll();
+    }
+    @Transactional
+    public void deleteByIdBorne(Integer id) {
+        borneRepository.deleteById(id);
+    }
+    @Transactional
+    public void saveBorne(Borne borne) {
+        borneRepository.save(borne);
+    }
+
     
 }
